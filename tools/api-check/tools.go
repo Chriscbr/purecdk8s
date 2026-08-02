25e5dd4de2e095f2f0b8f41d3d3ddc0d0d5386c5
Chris Rybicki <crybicki98@gmail.com>
2026-08-01 20:22:17 -0400

    add constructs API compatibility check

---
+++ tools/api-check/tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+
+// Package apicheck records the API comparison tool and its inputs.
+package apicheck
+
+import (
+	_ "github.com/aws/constructs-go/constructs/v10"
+	_ "github.com/purecdk8s/purecdk8s/constructs/v10"
+	_ "golang.org/x/exp/cmd/apidiff"
+)
